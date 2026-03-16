053aee8722c25451b85b6f1878644db5459433d0
carnevio <nevio@carcangiu.net>
2026-03-16 11:14:15 +0100

    add additional tests for Fraction operations and parsing

--- fraction_test.ts
+++ fraction_test.ts
@@ -34,3 +34,51 @@ Deno.test("1/3 + 2/6 = 2/3 is roughly 0.67", () => {
   // Assert
   assertAlmostEquals(left.toFloat(0.01), 0.67);
 });
+
+Deno.test("1/2 - 1/4 = 2/8", () => {
+  // Arrange
+  const left = new Fraction(1, 2);
+  const right = new Fraction(1, 4);
+
+  // Act
+  left.subtract(right);
+
+  // Assert
+  assertAlmostEquals(left.toFloat(0.01), 0.25);
+});
+
+Deno.test("1/2 * 2/3 = 2/6", () => {
+  // Arrange
+  const left = new Fraction(1, 2);
+  const right = new Fraction(2, 3);
+
+  // Act
+  left.multiply(right);
+
+  // Assert
+  assertAlmostEquals(left.toFloat(0.01), 0.33);
+
+});
+
+Deno.test("1/2 / 2/3 = 3/4", () => {
+  // Arrange
+  const left = new Fraction(1, 2);
+  const right = new Fraction(2, 3);
+
+  // Act
+  left.divide(right);
+
+  // Assert
+  assertAlmostEquals(left.toFloat(0.01), 0.75);
+});
+
+Deno.test("parse returns Fraction for '5/8'", () => {
+  // Act
+  const fraction = Fraction.parse("5/8");
+
+  // Assert
+  assertAlmostEquals(fraction.toFloat(0.01), 0.63);
+});
+
+
+
