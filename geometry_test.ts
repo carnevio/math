f2200b7b3ef2d3c91c63be183b761b32e81f5e1e
carnevio <nevio@carcangiu.net>
2026-03-23 07:56:23 +0100

    fix(Rectangle): correct circumference calculation and add tests for Rectangle methods

--- geometry_test.ts
+++ geometry_test.ts
@@ -1,5 +1,5 @@
-import { assertAlmostEquals } from "@std/assert";
-import { Circle, Point2D } from "./geometry.ts";
+import { assertAlmostEquals, assertEquals } from "@std/assert";
+import { Rectangle, Circle, Point2D } from "./geometry.ts";
 
 Deno.test("circumference of a circle with radius 5 is roughtly 31.416", () => {
   // Given
@@ -11,3 +11,29 @@ Deno.test("circumference of a circle with radius 5 is roughtly 31.416", () => {
   // Then
   assertAlmostEquals(actual, 31.416, 0.01);
 });
+
+Deno.test("area of rectangle (0,0)-(2,3) is 6", () => {
+  const rect = new Rectangle(new Point2D(0, 0), new Point2D(2, 3));
+  assertEquals(rect.area(), 6);
+});
+
+Deno.test("circumference of rectangle (0,0)-(2,3) is 10", () => {
+  const rect = new Rectangle(new Point2D(0, 0), new Point2D(2, 3));
+  assertEquals(rect.circumference(), 10);
+});
+
+Deno.test("diagonal of rectangle (0,0)-(2,3) is roughly 3.61", () => {
+  const rect = new Rectangle(new Point2D(0, 0), new Point2D(2, 3));
+  assertAlmostEquals(rect.diagonal(), 3.61, 0.01);
+});
+
+Deno.test("diameter of circle with radius 5 is 10", () => {
+  const circle = new Circle(new Point2D(0, 0), 5);
+  assertEquals(circle.diameter(), 10);
+});
+
+Deno.test("area of circle with radius 5 is roughly 78.54", () => {
+  const circle = new Circle(new Point2D(0, 0), 5);
+  assertAlmostEquals(circle.area(), 78.54, 0.01);
+});
+
